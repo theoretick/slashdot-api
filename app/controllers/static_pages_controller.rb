b7ef41593146a4b222eefc6376b21a5485d87d40
theoretick <me@lucascharles.me>
2013-11-02 13:21:32 -0700

    Minor index page bug fixes and added second example link.
    Changed title to SlashdotAPI from DiscussIt. Added latest-archived
    example return. Minor wording tweaks.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 
 class StaticPagesController < ApplicationController
   def index
+    latest_archived_url = Url.order('updated_at').last.target_url
+    @latest_url = "/slashdot_postings/search?url=#{latest_archived_url}"
   end
 
   def about
