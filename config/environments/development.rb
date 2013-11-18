3bf7d3000e88be1965ab5581ff5ad7ad7f408668
theoretick <me@lucascharles.me>
2013-11-17 19:29:31 -0800

    added mailer support, sendgrid in production, mailcatcher in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,5 +28,13 @@ DiscussIt::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+ config.assets.debug = true
+
+  #Mail catcher config
+  #NOTE: must add the ruby bin to your PATH
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
 end
