e69915d219eb500875e47f4f1e318da17073ded3
theoretick <me@lucascharles.me>
2013-12-08 13:24:17 -0800

    (no slip) added full display of latest posting to index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
   def index
     latest_archived_url = Url.order('updated_at').last.target_url
     @latest_url = "/slashdot_postings/search?url=#{latest_archived_url}"
+    @latest_json = SlashdotPosting.last.to_json
   end
 
   def about
