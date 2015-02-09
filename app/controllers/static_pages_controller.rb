b3603759f29194a0c019b78b8eb84d8d21ab8709
theoretick <me@lucascharles.me>
2015-02-08 18:26:03 -0800

    (no slip) Better homepage, display last 10 archived urls

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
   def index
     latest_archived_url = Url.order('updated_at').last.target_url
     @latest_url = "/slashdot_postings/search?url=#{latest_archived_url}"
+    @latest_10 = Url.order('updated_at').joins(:slashdot_postings).last(10)
     @latest_json = SlashdotPosting.last.to_json
   end
 
